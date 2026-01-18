d97c12e83ed5e8191584593ebfb781a60d3715bf
greed-106 <ymj20040312@gmail.com>
2026-01-18 14:44:33 +0800

    新的示例demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include "io/PlyReader.hpp"
 #include "io/PlyWriter.hpp"
 #include "utils/Timer.hpp"
+#include "codec/MortonOrder.hpp"
+#include "codec/Transform.hpp"
+#include "codec/Quantization.hpp"
 
 int main(int argc, char **argv) {
-    TICK(load);
+
     auto data = PlyReader::readDataFromFile("G:\\code\\cpp\\gaussian-stream\\test.ply");
-    data.printElementInfos();
-    TOCK(load);
-    TICK(save_text);
-    PlyWriter::writeDataToFile("G:\\code\\cpp\\gaussian-stream\\out-text.ply", data, PlyFormat::ASCII);
-    TOCK(save_text);
-    TICK(save_binary);
-    PlyWriter::writeDataToFile("G:\\code\\cpp\\gaussian-stream\\out-binary.ply", data, PlyFormat::BINARY_LITTLE_ENDIAN);
-    TOCK(save_binary);
-    
+
+    auto positions = data.getTypedProperties<float>("vertex", {"x", "y", "z"});
+
+    TICK(morton);
+    auto bbox = BoundingBox3D::calculateFromPoints(positions);
+    auto indices = MortonEncoder::calculateMortonIndices<uint64_t>(
+        Quantization::quantizePositionWithBBox<uint32_t, float, mortonnd::MortonNDBmi<3, uint64_t>::FieldBits>(positions, bbox)
+    );
+    Transform::logTransformInPlace(positions, bbox);
+    auto quantizedPositions = Quantization::quantizePositionWithBBox<uint16_t, float, 16>(positions, bbox);
+    TOCK(morton);
+
+    std::vector<std::vector<float>> dequantizedPositions(3, std::vector<float>(positions[0].size()));
+    for(const auto& axis : {0,1,2}) {
+        for(size_t i = 0; i < positions[0].size(); ++i) {
+            dequantizedPositions[axis][i] = static_cast<float>(quantizedPositions[axis][i]);
+        }
+    }
+    data.setProperties("vertex", {"x", "y", "z"}, dequantizedPositions);
+    PlyWriter::writeDataToFile("G:\\code\\cpp\\gaussian-stream\\out-withoutlog.ply", data, PlyFormat::BINARY_LITTLE_ENDIAN);
+
     return 0;
 }
