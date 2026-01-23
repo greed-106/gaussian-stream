0b5e8ff324db0f82d366b21e6b6488b1ca11de21
greed-106 <ymj20040312@gmail.com>
2026-01-23 18:33:30 +0800

    调用Draco进行几何编码的示例代码

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,106 @@
 #include "codec/MortonOrder.hpp"
 #include "codec/Transform.hpp"
 #include "codec/Quantization.hpp"
+#include <cstdint>
+
+const std::string ROOT_PATH = "G:\\code\\cpp\\gaussian-stream\\";
+const std::string INPUT_PATH = "G:\\code\\icip2026\\datasets\\coffee_martini_origin_ply_\\";
+const std::string ENCODED_PLY_PATH = ROOT_PATH + "output\\encoded-ply\\";
+const std::string ENCODED_DRC_PATH = ROOT_PATH + "output\\encoded-drc\\";
+const std::string DECODED_PLY_PATH = ROOT_PATH + "output\\decoded-ply\\";
+const std::string DRACO_ENCODER = "G:\\code\\cpp\\gaussian-stream\\draco_encoder.exe";
+const std::string DRACO_DECODER = "G:\\code\\cpp\\gaussian-stream\\draco_decoder.exe";
+
+void dracoEncode(const std::string& inputFile, const std::string& outputFile, int cl = 10, int qp = 16) {
+    std::string command = DRACO_ENCODER + " -i " + inputFile + " -o " + outputFile + " -qp " + std::to_string(qp) + " -cl " + std::to_string(cl);
+    auto res = std::system(command.c_str());
+    if(res != 0) {
+        SPDLOG_ERROR("Draco encoding failed for file: {}", inputFile);
+    }
+}
+void dracoDecode(const std::string& inputFile, const std::string& outputFile) {
+    std::string command = DRACO_DECODER + " -i " + inputFile + " -o " + outputFile;
+    auto res = std::system(command.c_str());
+    if(res != 0) {
+        SPDLOG_ERROR("Draco decoding failed for file: {}", inputFile);
+    }
+}
 
 int main(int argc, char **argv) {
 
-    auto data = PlyReader::readDataFromFile("G:\\code\\cpp\\gaussian-stream\\test.ply");
+    auto files = FileTools::findFilesMatchingPattern(INPUT_PATH, R"(.*\.ply)");
+    SPDLOG_INFO("Found {} PLY files in input directory.", files.size());
+    for(const auto& filePath : files) {
 
-    auto positions = data.getTypedProperties<float>("vertex", {"x", "y", "z"});
+        auto data = PlyReader::readDataFromFile(filePath.string());
+        auto positions = data.getTypedProperties<float>("vertex", {"x", "y", "z"});
+        auto attributes = data.getTypedProperties<float>("vertex", 
+            {"f_dc_0", "f_dc_1", "f_dc_2", 
+            "opacity","scale_0", "scale_1", "scale_2",
+            "rot_0", "rot_1", "rot_2", "rot_3"});
+        // 编码的流程
+        auto bbox = BoundingBox3D::calculateFromPoints(positions);
 
-    TICK(morton);
-    auto bbox = BoundingBox3D::calculateFromPoints(positions);
-    auto indices = MortonEncoder::calculateMortonIndices<uint64_t>(
-        Quantization::quantizePositionWithBBox<uint32_t, float, mortonnd::MortonNDBmi<3, uint64_t>::FieldBits>(positions, bbox)
-    );
-    Transform::logTransformInPlace(positions, bbox);
-    auto quantizedPositions = Quantization::quantizePositionWithBBox<uint16_t, float, 16>(positions, bbox);
-    TOCK(morton);
+        // 变换量化
+        Transform::logTransformInPlace(positions, bbox);
+        auto quantizedPositions = Quantization::quantizePositionWithBBox<uint16_t, float, 16>(positions, bbox);
+        
+        // 计算莫顿序
+        auto indices = MortonEncoder::calculateMortonIndices<uint64_t>(
+            Quantization::castVectors<uint32_t>(quantizedPositions)
+        );
 
-    std::vector<std::vector<float>> dequantizedPositions(3, std::vector<float>(positions[0].size()));
-    for(const auto& axis : {0,1,2}) {
-        for(size_t i = 0; i < positions[0].size(); ++i) {
-            dequantizedPositions[axis][i] = static_cast<float>(quantizedPositions[axis][i]);
+        // 对量化后的数据重排莫顿序
+        for(auto& position : quantizedPositions){
+            Transform::sortInPlaceWithIndices(position, indices);
         }
+        
+        for(auto& attr : attributes){
+            Transform::sortInPlaceWithIndices(attr, indices);
+        }
+
+        // 写入几何信息的PLY码流
+        auto encodedPlyFilePath = ENCODED_PLY_PATH + filePath.filename().string();
+        auto quantizedPositionsFP32 = Quantization::castVectors<float>(quantizedPositions);
+        data.setProperties("vertex", {"x", "y", "z"}, quantizedPositionsFP32);
+        PlyWriter::writeDataToFileWithPropertyMasks(encodedPlyFilePath, data, {"x", "y", "z"});
+
+        // 使用Draco压缩几何信息
+        auto dracoEncodedFilePath = ENCODED_DRC_PATH + filePath.stem().string() + ".drc";
+        dracoEncode(encodedPlyFilePath, dracoEncodedFilePath, 10, 14);
+        // 使用Draco解压缩几何信息
+        auto dracoDecodedPlyFilePath = DECODED_PLY_PATH + filePath.filename().string();
+        dracoDecode(dracoEncodedFilePath, dracoDecodedPlyFilePath);
+
+        // 读取解码后的几何信息
+        auto decodedData = PlyReader::readDataFromFile(dracoDecodedPlyFilePath);
+        auto decodedQuantizedPositions = decodedData.getTypedProperties<float>("vertex", {"x", "y", "z"});
+        // auto decodedQuantizedPositions = quantizedPositionsFP32; // 使用原始的量化数据进行反量化反变换测试
+
+        // 计算解码后数据的莫顿序
+        auto decodedIndices = MortonEncoder::calculateMortonIndices<uint64_t>(
+            Quantization::castVectors<uint32_t>(decodedQuantizedPositions)
+        );
+        // 重排序
+        for(auto& position : decodedQuantizedPositions){
+            Transform::sortInPlaceWithIndices(position, decodedIndices);
+        }
+        
+        // 反量化反变换
+        auto dequantizedPositions = Quantization::dequantizePositionWithBBox<float, float, 16>(decodedQuantizedPositions, bbox);
+        Transform::inverseLogTransformInPlace(dequantizedPositions, bbox);
+        
+        data.setProperties("vertex", {"x", "y", "z"}, dequantizedPositions);
+        data.setProperties("vertex", 
+            {"f_dc_0", "f_dc_1", "f_dc_2", 
+            "opacity","scale_0", "scale_1", "scale_2",
+            "rot_0", "rot_1", "rot_2", "rot_3"}, attributes);
+        
+        // 保存最终解码结果
+        auto finalDecodedPlyFilePath = DECODED_PLY_PATH + filePath.filename().string();
+        PlyWriter::writeDataToFile(finalDecodedPlyFilePath, data);
+        // PlyWriter::writeDataToFileWithPropertyMasks(finalDecodedPlyFilePath, data, {"x", "y", "z"});
     }
-    data.setProperties("vertex", {"x", "y", "z"}, dequantizedPositions);
-    PlyWriter::writeDataToFile("G:\\code\\cpp\\gaussian-stream\\out-withoutlog.ply", data, PlyFormat::BINARY_LITTLE_ENDIAN);
 
     return 0;
 }
