a87911f5044ccd48bddd1790908318b6af50513a
greed-106 <ymj20040312@gmail.com>
2026-01-26 13:39:13 +0800

    添加了2D莫顿序解码的方法

--- include/codec/MortonOrder.hpp
+++ include/codec/MortonOrder.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <morton-nd/mortonND_BMI2.h>
+#include <tuple>
 #include <vector>
 #include <stdexcept>
 #include <numeric>
@@ -10,20 +11,20 @@
 // Morton编码辅助类，基于morton-nd库实现
 class MortonEncoder {
 public:
-    template<typename T = uint64_t>
-    static std::vector<T> calculateMortonIndices(const std::vector<std::vector<uint32_t>>& coordinates) {
+    template<typename IndicesType = uint64_t, typename CoordinateType = uint32_t>
+    static std::vector<IndicesType> encode3DMortonIndices(const std::vector<std::vector<CoordinateType>>& coordinates) {
         const size_t Dimensions = 3;
         if(coordinates.size() != Dimensions) {
             throw std::runtime_error("Dimension mismatch in calculateMortonIndices");
         }
 
         size_t numPoints = coordinates[0].size();
-        std::vector<T> mortonIndices(numPoints);
-        std::vector<T> indices(numPoints);
+        std::vector<IndicesType> mortonIndices(numPoints);
+        std::vector<IndicesType> indices(numPoints);
 
         std::iota(indices.begin(), indices.end(), 0);
 
-        using MortonND = mortonnd::MortonNDBmi<Dimensions, T>;
+        using MortonND = mortonnd::MortonNDBmi<Dimensions, IndicesType>;
         // auto [quantizedPositions, bbox] = Quantization::quantizePosition<uint32_t, float, MortonND::FieldBits>(coordinates);
 
         for(size_t i = 0; i < numPoints; ++i) {
@@ -34,11 +35,23 @@ public:
         }
 
         std::sort(indices.begin(), indices.end(),
-            [&mortonIndices](T a, T b) {
+            [&mortonIndices](IndicesType a, IndicesType b) {
                 return mortonIndices[a] < mortonIndices[b];
             }
         );
 
         return indices;
     }
+};
+
+class MortonDecoder{
+public:
+    template<typename T>
+    static std::tuple<T, T> decode2DMortonIndex(T mortonIndex) {
+        const size_t Dimensions = 2;
+        using MortonND = mortonnd::MortonNDBmi<Dimensions, T>;
+
+        return MortonND::Decode(mortonIndex);
+    }
+
 };
